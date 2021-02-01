a49c1844eb3f229c56ddd07ced16b8ad8130b027
CamRobinson <robinsoncameron1103@gmail.com>
2021-02-01 08:48:31 -0600

    Added to Player.cpp and Player.h

--- raygame/Player.h
+++ raygame/Player.h
@@ -1,5 +1,26 @@
-#pragma once
+#include "Actor.h"
+#include<string>
 class Player
 {
+public:
+    void player(std::string, std::string);
+    void setName(std::string);
+    void setArea(std::string);
+    void setMaxHealth();
+    void setDamage();
+    std::string getName();
+    std::string getArea();
+    void setHealth(double);
+    double getHealth();
+    double getMaxHealth();
+    int getDamage();
+
+private:
+    std::string playerName;
+    std::string playerArea;
+    int playerLevel;
+    double playerHealth;
+    double playerMaxHealth;
+    int playerDamage;
 };
 
